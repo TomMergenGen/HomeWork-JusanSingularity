0392aef8f2c7d88a8565287b4fce9b41d6b0c7fb
TomMergenGen <65769183+TomMergenGen@users.noreply.github.com>
2024-04-11 14:52:55 +0600

    create class HomeWorkApp created main create method printThreeWords create method checkSumSign create method printColor create method compareNumbers

---
+++ src/main/java/kz/tomgeni/HomeWorkApp.java
@@ -0,0 +1,50 @@
+package kz.tomgeni;
+
+public class HomeWorkApp {
+    // Задание 1.
+    public static void main(String[] args) {
+        // Задание 6.
+        printThreeWords();
+        checkSumSign(-3,2);
+        printColor(101);
+        compareNumbers(-1,-1);
+    }
+
+    // Задание 2.
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    // Задание 3.
+    static void checkSumSign (int a, int b){
+        int sum = a+b;
+        if (sum>=0){
+            System.out.println("Сумма положительна");
+        } else {
+            System.out.println("Сумма отрицательна");
+        }
+    }
+
+    // Задание 4.
+    static void printColor (int value){
+        if (value <= 0){
+            System.out.println("Красный");
+        }else if (value <= 100){
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    // Задание 5.
+    static void compareNumbers (int a, int b){
+        if (a>=b){
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+
+}
